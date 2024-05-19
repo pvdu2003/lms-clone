f5fe96979ea803f3a304df437b1ea40c49e85204
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2024-05-19 16:06:51 +0700

    Change something on home route

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -2,11 +2,6 @@ const Announcements = require("../models/Announcements");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 class HomeController {
-  renderLoginForm(req, res) {
-    const user = req.cookies.user;
-    let err = {};
-    res.render("pages/home", { user: user ? user : undefined, err });
-  }
   // POST /
   login(req, res, next) {
     const { username, password } = req.body;
@@ -18,43 +13,36 @@ class HomeController {
       err.password = "Please enter your password";
     }
     if (Object.keys(err).length > 0) {
-      res.status(400);
-      console.log(req.cookies.user);
-      res.render("pages/login", { err });
-    } else {
-      User.findOne({ username })
-        .then((existingUser) => {
-          if (!existingUser) {
-            err.username = "This username is not existing!";
-            res.render("pages/login", { err });
-            // res.status(400).send('This username has not existed! Please try again!');
-          } else {
-            bcrypt
-              .compare(password, existingUser.password)
-              .then((match) => {
-                if (match) {
-                  res.cookie("user", existingUser, { maxAge: 1800000 });
-                  // res.send(existingUser._id)
-                  res.redirect("/home");
-                  // res.render('pages/home', {user});
-                } else {
-                  err.password = "This password is not correct!";
-                  res.render("pages/login", { err });
-                  // res.status(400).send('Your password is not correct! Please try again!');
-                }
-              })
-              .catch(next);
-          }
-        })
-        .catch(next);
+      return res.render("pages/login", { err });
     }
+    User.findOne({ username })
+      .then((existingUser) => {
+        if (!existingUser) {
+          err.username = "This username is not existing!";
+          return res.render("pages/login", { err });
+        } else {
+          bcrypt
+            .compare(password, existingUser.password)
+            .then((match) => {
+              if (match) {
+                res.cookie("user", existingUser, { maxAge: 1800000 });
+                res.redirect("/");
+              } else {
+                err.password = "This password is not correct!";
+                res.render("pages/login", { err });
+              }
+            })
+            .catch(next);
+        }
+      })
+      .catch(next);
   }
   // POST /logout
   logout(req, res, next) {
     res.clearCookie("user");
     res.redirect("/");
   }
-  // GET /home
+  // GET /
   async goHome(req, res, next) {
     try {
       const user = req.cookies.user;
@@ -88,16 +76,13 @@ class HomeController {
         .then((existingUser) => {
           if (existingUser) {
             err.username = "This username has already existed!";
-            // return res.status(400).send('Username already exists!');
           }
           if (password.length <= 6) {
             err.password = "This password is too short!";
-            // return res.status(400).send('This password is too short! Please choose another password');
           }
           if (password !== confirmPw) {
             err.confirmPw =
               "This password is not match with the previous password above!";
-            // return res.status(400).send('Please enter correct password again');
           }
           if (Object.keys(err).length > 0) {
             res.render("pages/signin", { err });
--- routes/home.route.js
+++ routes/home.route.js
@@ -1,14 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const homeController = require('../app/controllers/home.controller');
-const authenticateUser = require('../app/middlewares/authenticateUser');
+const homeController = require("../app/controllers/home.controller");
 
-router.get('/signin', homeController.renderSigninForm);
-router.post('/signin', homeController.signin);
-router.get('/home', authenticateUser,  homeController.goHome);
-router.get('/logout', homeController.logout);
-router.get('/', homeController.renderLoginForm);
-router.post('/', homeController.login);
-
+router.get("/signin", homeController.renderSigninForm);
+router.post("/signin", homeController.signin);
+router.get("/logout", homeController.logout);
+router.get("/", homeController.goHome);
+router.post("/", homeController.login);
 
 module.exports = router;
